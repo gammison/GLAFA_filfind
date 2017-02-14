bf556e1a2aacaa9d3d848e3c36a53c3c584e3859
LLi1996 <larry9607@gmail.com>
2017-02-14 00:54:13 -0500

    commented & fixed mask_obj_node.py

--- mask_obj_node.py
+++ mask_obj_node.py
@@ -2,6 +2,16 @@ import numpy as np
 
 class MaskObjNode:
 	'''
+	This node object is made to contain the masks produced by fil_finder.
+	Each node takes in a mask(square), the corners of the mask, and the v index,
+	other fields are then calculted.
+
+	It is important to note the differnce between the area of a mask and the
+	masked area: since the masks are aways squares, the area of a mask is just
+	calculated from dimentions of the corners. The masked area, however, is 
+	calculated by counting the amount of pixels that's masked (marked TRUE in 
+	the np array).
+
 	'''
 
 	def __init__(self, mask_obj, corners, v_slice_index):
@@ -10,6 +20,7 @@ class MaskObjNode:
 		
 		# corners are [(bottomleft)[y,x],(topright)[y,x]]
 		# organize corners in to list of lists instead of list of tups
+		# and convert into [x,y]
 		self.corner_BL = [corners[0][1], corners[0][0]]
 		self.corner_TR = [corners[1][1], corners[1][0]]
 		self.corners = [self.corner_BL, self.corner_TR]
@@ -23,6 +34,12 @@ class MaskObjNode:
 
 
 	def mergeNode(self, other_node):
+		'''
+		Merge other_node with self. Append to self.v_slice_index if needed and 
+		create the combined OR mask of self.mask and other_node.mask. The 
+		combined or mask is set as the new self.mask and other attributes are
+		fixed
+		'''
 		if self.v_slice_index[-1] != other_node.v_slice_index[0]:
 			self.v_slice_index.append(other_node.v_slice_index)
 
@@ -41,6 +58,9 @@ class MaskObjNode:
 
 
 	def mergeNodeAlt(self, other_node):
+		'''
+		alt mergeNode function
+		'''
 		'''
 		comboMask = [[0 for x in range(maxBR_X-TL_X)] for y in range(BR_Y - TL_Y)]
 		radj_1 = TL_Y - mask1.leftcorner.y
@@ -63,6 +83,15 @@ class MaskObjNode:
 
 
 	def checkMaskOverlap(self, other_node, overlap_thresh):
+		'''
+		First check if the coners of self.mask and other_node.mask overlap, if
+		the coners do overlap (meaning some part of the two squares overlap) we 
+		then check if actual masks overlap. To check for actual overlaps a 
+		combined AND mask is first made, and the masked area calculated. The 
+		masked area of the combined AND mask is then compared to the masked area
+		of the imput masks. If the overlap is greater than overlap_thresh (# of
+		pixels) then return True
+		'''
 		if self.checkCornersOverlap(other_node) == False:
 			return False
 
@@ -78,6 +107,14 @@ class MaskObjNode:
 
 
 	def combineMask(self, other_node, merge_type='AND'):
+		'''
+		Merge self.mask and other_node.mask. The corners of the 2 masks are
+		first matched to fined the smallest square that contains both masks,
+		a new mask is then made to that dimention. Elements of the new mask are
+		filled basked on the merge_type ('AND' or 'OR').
+
+		Return the new combined mask
+		'''
 		new_corners = self.matchCorners(other_node)
 		r_dim = new_corners[1][1] - new_corners[0][1]
 		c_dim = new_corners[1][0] - new_corners[0][0]
@@ -96,6 +133,9 @@ class MaskObjNode:
 
 
 	def checkCornersOverlap(self, other_node):
+		'''
+		Check if self.mask and other_node.mask have any overlap
+		'''
 		if other_node.corner_BL[0] >= self.corner_TR[0] or \
 			other_node.corner_TR[0] <= self.corner_BL[0]:
 			return False
@@ -107,6 +147,12 @@ class MaskObjNode:
 
 
 	def matchCorners(self, other_node):
+		'''
+		Compare the self.mask and other_node.mask and pick out the smallest
+		square that contain both masks.
+
+		Return the corners of that square.
+		'''
 		BL_X = min(self.corner_BL[0], other_node.corner_BL[0])
 		BL_Y = min(self.corner_BL[1], other_node.corner_BL[1])
 
@@ -117,6 +163,12 @@ class MaskObjNode:
 
 
 	def checkMaskedAreaSize(self, mask=None):
+		'''
+		Calculate the amount of pixels that are masked by self.mask. If a new
+		mask is provided then that mask is used
+
+		Return the # of pixels masked (as opposed to the area of the mask)
+		'''
 		if mask == None:
 			mask = self.mask
 
@@ -124,6 +176,13 @@ class MaskObjNode:
 
 
 	def checkAreaSize(self, corners=None):
+		'''
+		Calculate the area of self.mask by looking at its dimentions from its
+		corners. If corners are provided then an area based on that corner is 
+		calculated.
+
+		Return the area of the mask (in pixels^2)
+		'''
 		if corners == None:
 			corners = self.corners
 
@@ -131,6 +190,11 @@ class MaskObjNode:
 
 
 	def expandMask(self, new_corners):
+		'''
+		Expand self.mask so its corners match the new_corners provided. Masks
+		are paded with 0s with the numpy function pad()
+		Return new expanded mask
+		'''
 		mask = self.mask
 		old_corners = self.corners
 
